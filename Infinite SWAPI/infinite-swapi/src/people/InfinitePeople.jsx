6ca683d093a53408635e362da967b74945508007
MPS Vihanga <>
2024-09-29 14:38:13 +0530

    infinite scrolling

--- Infinite SWAPI/infinite-swapi/src/people/InfinitePeople.jsx
+++ Infinite SWAPI/infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,5 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { Person } from "./Person";
+import {useInfiniteQuery} from "@tanstack/react-query";
 
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
@@ -9,5 +10,12 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
+  const {data, fetchNextPage, hasNextPage} = useInfiniteQuery({
+    "sw-people",
+    ({pageParam = initialUrl}) => fetchUrl(pageParam),
+
+  });
+
+
   return <InfiniteScroll />;
 }
